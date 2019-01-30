6cef5e67e125455b2ce7fb82a2b05bac525ed5de
Pratikshit <pratikshitpfd@gmail.com>
2019-01-30 16:46:19 +0530

    bug with defined conditions

--- clientweather/src/App.js
+++ clientweather/src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import Landing from './Landing';
+import Weather from './Weather';
+import Form from './Form';
+
+import {BrowserRouter as Route} from 'react-router-dom';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  
+
+  getWeather = async (e) => {
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=lucknow&APPID=f3d181f6d7552e6a0a70f50699fbf7f0`);
+    
+    const response = await api_call.json();
+    e.preventDefault();
+    console.log(response);
+    
+  }
+
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Weather App</h1>
+        <Landing/>
+        <Form loadWeather={this.getWeather}/>
+        <Weather/>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ clientweather/src/Landing.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div >
+        <h1>Weather Landing</h1>
+        <p>Helps you find weather conditions in cities...</p>
+      </div>
+    );
+  }
+}
+
+export default Landing;
+
+
+
+
+
+
+
+
+
+
+
+
